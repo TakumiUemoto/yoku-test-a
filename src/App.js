da798e98ac401d297ec680b32898fedbbfc50c24
TakumiUemoto <119883036+TakumiUemoto@users.noreply.github.com>
2023-01-07 23:29:05 +0900

    axiosで文字列取得機能の追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(props){
 
-export default App;
+  const initialText = 'Cats have about 130,000 hairs per square inch (20,155 hairs per square centimeter).';
+  //関数コンポーネントが持つstateを定義　*const [状態変数, 状態変数の更新関数] = useState(初期値)
+  //useStateの引数にはstateの初期値が入る　
+  const [post, setPost] = useState(initialText);
+  const onButtonClick = () => {
+    const newText = randomFact;
+    setPost(newText);
+  }
+
+  //axios
+  useEffect(() => {
+    axios
+      .get('https://catfact.ninja/fact') 
+      .then(res => {
+        setPost(res.data);
+      });
+    }, []);
+  if(!post) return null;
+
+  const randomFact = post.fact;
+  const style = {display: 'flex',alignItems: 'center', textAlign: 'left', fontSize: '28px',};
+  
+    return (
+      <div className="App" {...{style}}>
+        <p >{post}</p> 
+        <button onClick={onButtonClick}>更新</button>
+      </div>
+    );
+  
+}
