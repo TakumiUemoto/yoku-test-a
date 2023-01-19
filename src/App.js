2985ada7165b8bd3e50b56663e7e5cf406b68865
TakumiUemoto <119883036+TakumiUemoto@users.noreply.github.com>
2023-01-20 00:13:06 +0900

    整理した

--- src/App.js
+++ src/App.js
@@ -1,36 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-export default function App(props){
+const baseURL = "https://catfact.ninja/fact";
+const initialFact = "Cats have about 130,000 hairs per square inch (20,155 hairs per square centimeter).";
 
-  const initialText = 'Cats have about 130,000 hairs per square inch (20,155 hairs per square centimeter).';
-  //関数コンポーネントが持つstateを定義　*const [状態変数, 状態変数の更新関数] = useState(初期値)
-  //useStateの引数にはstateの初期値が入る　
-  const [post, setPost] = useState(initialText);
-  const onButtonClick = () => {
-    const newText = randomFact;
-    setPost(newText);
-  }
+export default function App() {
+    const [post, setPost] = React.useState(initialFact);
+    const [error, setError] = React.useState(null);
+    const renderFlgRef = React.useRef(false);
 
-  //axios
-  useEffect(() => {
-    axios
-      .get('https://catfact.ninja/fact') 
-      .then(res => {
-        setPost(res.data);
-      });
-    }, []);
-  if(!post) return null;
+    React.useEffect(() => {
+      if(renderFlgRef.current) {
+        axios.get(baseURL).then((res) => {
+            setPost(res.data);
+            console.log(res.data);
+        }).catch(error => {
+          setError(error);
+        });} else {
+          renderFlgRef.current = true;
+        }
+}, [post]);
 
-  const randomFact = post.fact;
-  const style = {display: 'flex',alignItems: 'center', textAlign: 'left', fontSize: '28px',};
+  if(error) return `Error: ${error.message}`;
+  if(!post) return "no Post!";
   
     return (
-      <div className="App" {...{style}}>
-        <p >{post}</p> 
-        <button onClick={onButtonClick}>更新</button>
+      <div>
+        <p>{post.fact}</p>
+        <p>{post.length}</p>
+        <button >更新</button>
       </div>
     );
-  
-}
+  }
