044abf15f3a0c4ed844a6bc53fb720b31a30ce52
TakumiUemoto <119883036+TakumiUemoto@users.noreply.github.com>
2023-01-30 14:23:03 +0900

    オブジェクトのfact要素指定して表示するようにした。 Aの機能は完成した

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const baseURL = "https://catfact.ninja/fact";
+const API_URL = "https://catfact.ninja/fact";
 const initialFact = "Cats have about 130,000 hairs per square inch (20,155 hairs per square centimeter).";
 
 export default function App() {
-    const [post, setPost] = React.useState(initialFact);
-    const [error, setError] = React.useState(null);
-    const renderFlgRef = React.useRef(false);
+  const [text, setText] = useState(initialFact);
+  const [isLoading, setLoading] = useState('');
 
-    React.useEffect(() => {
-      if(renderFlgRef.current) {
-        axios.get(baseURL).then((res) => {
-            setPost(res.data);
-            console.log(res.data);
-        }).catch(error => {
-          setError(error);
-        });} else {
-          renderFlgRef.current = true;
-        }
-}, [post]);
-
-  if(error) return `Error: ${error.message}`;
-  if(!post) return "no Post!";
-  
-    return (
-      <div>
-        <p>{post.fact}</p>
-        <p>{post.length}</p>
-        <button >更新</button>
-      </div>
-    );
+  async function handleClick() {
+    setLoading(true);
+    const response = await axios.get(API_URL);
+    setText(response.data.fact);
+    console.log(response.data.fact);
+    setLoading(false);
   }
+
+  
+
+  return (
+    <div>
+      {isLoading ? console.log('Text Loading...') : <h2>{text}</h2>}
+      <button onClick={handleClick} disabled={isLoading}>
+        更新
+      </button>
+    </div>
+  );
+}
