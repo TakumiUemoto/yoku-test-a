0eb839747d73e914091d0535048553422a17a916
TakumiUemoto <119883036+TakumiUemoto@users.noreply.github.com>
2023-02-16 11:56:59 +0900

    Styled-Componentsに書き換え

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './App.css';
+import styled from 'styled-components';
 
 const API_URL = "https://catfact.ninja/fact";
 const initialFact = "Cats have about 130,000 hairs per square inch (20,155 hairs per square centimeter).";
 
-export default function App() {
-  const [text, setText] = useState(initialFact);
-  const [isLoading, setLoading] = useState('');
+function App() {
+  const [ text, setText ] = useState( initialFact );
+  const [ isLoading, setLoading ] = useState('');
 
   async function handleClick() {
     setLoading(true);
-    const response = await axios.get(API_URL);
-    setText(response.data.fact);
-    console.log(response.data.fact);
+    const response = await axios.get( API_URL );
+    setText( response.data.fact );
+    console.log( response.data.fact );
     setLoading(false);
-  }
+  };
 
   
 
   return (
-    <div>
-      {isLoading ? console.log('Text Loading...') : <h2>{text}</h2>}
+    <Container>
+      {isLoading ? console.log('Text Loading...') : <Text>{text}</Text>}
       <button onClick={handleClick} disabled={isLoading}>
         更新
       </button>
-    </div>
+    </Container>
   );
-}
+};
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.p`
+  display: flex;
+  font-size: 28px;
+  width: 450px;
+  line-height: 1.4;
+`;
+
+export default App;
